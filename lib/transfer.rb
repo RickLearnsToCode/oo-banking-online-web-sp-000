12a7f4ee98225b9fdc99c2bd08b263a6b56cd8a2
Rick Rosetti <rick.rosetti@gmail.com>
2019-10-28 13:46:32 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,11 +1,7 @@
 class Transfer
   attr_reader :sender, :receiver, :amount, :status
 
-<<<<<<< HEAD
   def initialize(sender, receiver, amount)
-=======
-  def initialize(receiver, sender, amount)
->>>>>>> 06a4616eb222ab8720c5b32f141aff6b103af54b
     @sender = sender
     @receiver = receiver
     @amount = amount
@@ -18,12 +14,8 @@ class Transfer
 
   def execute_transaction
     if @sender.balance < @amount
-<<<<<<< HEAD
       @status = "rejected"
       "Transaction rejected. Please check your account balance."
-=======
-      "Transaction rejected. Plase check your account balance."
->>>>>>> 06a4616eb222ab8720c5b32f141aff6b103af54b
     elsif valid? && @status == "pending"
       @sender.deposit(-@amount)
       @receiver.deposit(@amount)
@@ -33,7 +25,6 @@ class Transfer
     end
   end
 
-<<<<<<< HEAD
   def reverse_transfer
     if @status == "complete"
       @sender.deposit(@amount)
